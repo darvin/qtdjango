e6c63b3efc5d14a65a16c5aa56a9c52394b0029c
Sergey Klimov <dcdarv@gmail.com>
2010-10-12 00:30:38 +0300

    post api models write

--- src/qtdjango/piston_handlers.py
+++ src/qtdjango/piston_handlers.py
@@ -5,22 +5,52 @@ This module must be imported from django enviroment
 
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from piston.handler import BaseHandler
+from piston.handler import AnonymousBaseHandler
+from piston.utils import rc
 from helpers import get_resource_name_for_model, get_all_models
 
+from django.db import models
 
-
-class CollectionHandler(BaseHandler):
+class CollectionHandler(AnonymousBaseHandler):
     exclude = ()
-    allowed_methods = ('GET',)
+    allowed_methods = ('GET','POST')
+    
+    def has_model(self):
+        return hasattr(self, 'model') or hasattr(self, 'queryset')
 
-    def read(self, request,  *args, **kwargs):
-        filterargs = {}
-        for arg in request.GET:
-            filterargs[arg] = request.GET[arg]
 
-        return self.model.objects.filter(**filterargs) 
 
+
+    def create(self, request, *args, **kwargs):
+        if not self.has_model():
+            return rc.NOT_IMPLEMENTED
+
+        # get the actual data that wants to be created from the api request
+        attrs = self.flatten_dict(request.POST)
+
+        for field in self.model._meta.fields:
+            if field.rel:
+                # this is a ForeignKey (as far as I can tell... _meta isn't documented)
+                # so we need to convert it to it's pk equivilent... there's most likely
+                # a better way of doing this than hardcoding stuff...
+                if field.name in attrs:
+                    try:
+                        attrs[field.name] =  field.rel.to.objects.get(\
+                            **{field.rel.field_name: attrs[field.name]})
+                    except:
+                        pass
+
+
+        try:
+            inst = self.model.objects.get(**attrs)
+            return rc.DUPLICATE_ENTRY
+        except self.model.DoesNotExist:
+            inst = self.model()
+            inst = self.model(**attrs)
+            inst.save()
+            return inst
+        except self.model.MultipleObjectsReturned:
+            return rc.DUPLICATE_ENTRY
 
 def get_url_pattens(app_list):
     """Gets app list returns urls patterns"""
@@ -31,6 +61,3 @@ def get_url_pattens(app_list):
  
 
     return patterns("", * urlpatterns)
-    
-    
-    
